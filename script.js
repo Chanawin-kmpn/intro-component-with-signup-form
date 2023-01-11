c318d503b4e6803f5be4b4aaa24c338c6bf6976d
Upond <pond_chanawin@hotmail.com>
2023-01-11 23:00:05 +0700

    Change some error style
    Change position error style to the right

--- script.js
+++ script.js
@@ -9,38 +9,50 @@ function submitClaim(event) {
     event.preventDefault();
     const input = document.querySelectorAll("input")
     input.forEach(e => {
+        function setInputError(e) {
+            e.style.borderColor = 'hsl(0, 100%, 74%)';
+        }
+        e.style.borderColor = '';
         let dataInput = e.getAttribute("data-input")
         if (!e.value) {
             if (dataInput == "First Name") {
-                errfirstname.innerText = `${dataInput} is empty`
+                errfirstname.innerText = `${dataInput} cannot be empty`
+                setInputError(e);
             }
             if (dataInput == "Last Name") {
-                errlastname.innerText = `${dataInput} is empty`
+                errlastname.innerText = `${dataInput} cannot be empty`
+                setInputError(e);
             }
             if (dataInput == "Email") {
-                erremail.innerText = `${dataInput} is empty`
+                erremail.innerText = `${dataInput} cannot be empty`
+                setInputError(e);
             }
             if (dataInput == "Password") {
-                errpassword.innerText = `${dataInput} is empty`
+                errpassword.innerText = `${dataInput} cannot be empty`
+                setInputError(e);
             }
         }
         else {
             if (dataInput == "First Name") {
                 errfirstname.innerText = ``
+                e.style.borderColor = '';
             }
             if (dataInput == "Last Name") {
                 errlastname.innerText = ``
+                e.style.borderColor = '';
             }
             if (dataInput == "Email") {
                 const emailCheck = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
                 if (emailCheck.test(e.value)) {
                     erremail.innerText = ``;
+                    e.style.borderColor = '';
                 } else {
-                    erremail.innerText = `Email is not valid.`;
+                    erremail.innerText = `Look like this is not an email`;
                 }
             }
             if (dataInput == "Password") {
                 errpassword.innerText = ``
+                e.style.borderColor = '';
             }
         }
     });
